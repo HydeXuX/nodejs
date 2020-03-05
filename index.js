f4531ccb98de38298d4864da2d46ef956072decf
HydeXuX <xuxiao3939@gmail.com>
2020-03-05 16:43:01 -0700

    dude

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/math', (req, res) => {
+        var result
+        var num1 = Number(req.query.num1)
+        var num2 = Number(req.query.num2)
+        if (req.query.type == 1) {
+            result = num1 + num2
+        } else if (req.query.type == 2) {
+            result = num1 - num2
+        } else if (req.query.type == 3) {
+            result = num1 * num2
+        } else if (req.query.type == 4) {
+            result = num1 / num2
+        }
+        res.send(200, result)
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
